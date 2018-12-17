3755807cca06a1f55ec339e0b02e732dc17199d7
Nathanael Joseph <nathanaeljy@outlook.com>
2018-12-17 16:41:37 +0200

    Added blueprint for merge_sort function

---
+++ MyLib.py
@@ -0,0 +1,9 @@
+def merge_sort(lst):
+    #split list into left and right.
+    
+    # sorted_left = merge_sort(left)
+    # sorted_right = merge_sort(right)
+
+    # osrted_list = merge(sorted_left, sorted_right)
+
+    # return sorted_list
