9586053425d95210a0792803f9196dfa0c893695
nathan yebgui <nathan_yebgui@msn.com>
2018-12-24 14:01:10 +0200

    dded the merge_sort fuinctions

--- MyLib.py
+++ MyLib.py
@@ -1,9 +1,46 @@
-def merge_sort(lst):
-    #split list into left and right.
-    
-    # sorted_left = merge_sort(left)
-    # sorted_right = merge_sort(right)
+def merge_sort (lst):
+    """
+    Sorts a list into ascending order.
+    returns a new sorted list.
+    1 - divide - find the mindpoint of the list and divite into sublists.
+    2 - conquer - recursively sort the sublists from the previous step.
+    3 - combine - merge the sublists from the previous steps.
+    """
+    if len(lst) <= 1 :
+        return lst
 
-    # osrted_list = merge(sorted_left, sorted_right)
+    left_half, right_half = split(lst)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
 
-    # return sorted_list
+    return merge(left, right)
+
+def split(lst):
+    """
+    Divides the unsorted list into sublists at midpoint.
+    Returns two sublists, left and right.
+    """
+    mid = len(lst)/2
+    left = lst[:mid]
+    right = lst[mid:]
+
+    return left, right
+
+def merge(left, right):
+    """
+    Merges two lists (left and right), sorting them in the process.
+    Returns a new sorted list.
+    """
+
+    l = []
+    i = 0 
+    j = 0 
+
+    while i < len(left) and  j < len(right):
+        if left[i] < right[j]:
+            l.append(left[i])
+            i += 1
+        else:
+            l.append(right[j])
+            j += 1
+
